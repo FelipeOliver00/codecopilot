ccf7a3348499a19bfa3293fe1e2abe22a7d2572d
Felipe <felipecastro0208@hotmail.com>
2024-01-11 17:31:28 -0300

    Little adjustment with annotation @Repository

--- src/main/java/com/lipe/copilote/repository/CustomerRepository.java
+++ src/main/java/com/lipe/copilote/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.lipe.copilote.repository;
 
 import com.lipe.copilote.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 }
